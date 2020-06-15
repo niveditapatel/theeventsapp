f68aed51891b61c3af4a683c43cc70392cbc63a7
Nivedita Patel <niveditapatel@Niveditas-MacBook-Air.local>
2020-06-15 11:31:08 +0530

    with update event

---
+++ src/main/java/com/eventmanagement/eventmanagement/entity/UpdatedEvent.java
@@ -0,0 +1,31 @@
+package com.eventmanagement.eventmanagement.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UpdatedEvent {
+
+
+
+    private int event_id;
+    private String title;
+    private String description;
+    private String place;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date startDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    private Date endDateTime;
+
+}
